a04c2143f47074920bac3864fa2f85ac6561f056
Timothe Lebrun <timothe463@icloud.com>
2024-09-27 09:55:58 +0200

    style-header: stylize header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,11 +5,13 @@ function Header() {
    const path = useLocation().pathname;
     return (
         <>
-            <header className="header section_py">
+            <header className="header">
                 <img className="header_logo" src={logo} />
                 <ul className="header_linkswp">
-                    <li className="header_linkswp_item"><a className={`header_linkswp_item_link ${path === '/' && 'header_linkswp_item_link_active'}`} href="/">Accueil</a></li>
-                    <li className="header_linkswp_item"><a className={`header_linkswp_item_link ${path === '/a-propos' && 'header_linkswp_item_link_active'}`}  href="/a-propos">À propos</a></li>
+                    <li className="header_linkswp_item"><a className={`header_linkswp_item_link`} href="/">Accueil</a></li>
+                    <li className="header_linkswp_item"><a className={`header_linkswp_item_link`}  href="/">Profil</a></li>
+                    <li className="header_linkswp_item"><a className={`header_linkswp_item_link`}  href="/">Réglages</a></li>
+                    <li className="header_linkswp_item"><a className={`header_linkswp_item_link`}  href="/">Communauté</a></li>
                 </ul>
             </header>
         </>
