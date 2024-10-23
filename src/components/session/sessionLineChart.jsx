75a0438566999271870ce05d21360e18e4f8b2e5
Timothe Lebrun <timothe463@icloud.com>
2024-10-23 14:56:41 +0200

    feat-session: add gradient line & hover effect; fix-sidebar: stretch sidebar

--- src/components/session/sessionLineChart.jsx
+++ src/components/session/sessionLineChart.jsx
@@ -1,62 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-const data = [
-    {
-        name: 'Page A',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
-    },
-    {
-        name: 'Page B',
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: 'Page C',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
-    },
-    {
-        name: 'Page D',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
-    },
-    {
-        name: 'Page E',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
-    },
-    {
-        name: 'Page F',
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
-    },
-    {
-        name: 'Page G',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-    {
-        name: 'Page G',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-    {
-        name: 'Page G',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-];
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Rectangle, ResponsiveContainer } from 'recharts';
 
 export default function SessionLineChart({userSessions}) {
     const [sessions, setSessions] = useState([]);
@@ -72,28 +15,56 @@ export default function SessionLineChart({userSessions}) {
     function getXAxisDays() {
         return ["","L", "M", "M", "J", "V", "S", "D",""];
     }
-            console.log(sessions)
     function getSession(sessions){
         const days = getXAxisDays();
         return sessions.map((session,index) => {
            return {day: days[index + 1],value: session.sessionLength}
         })
     }
+    const CustomTooltip = ({ active, payload }) => {
+        if (active && payload && payload.length) {
+            return (
+                <div className='session-tooltip'>
+                    <p className='session-tooltip-value'>{`${payload[0].value} min`}</p>
+                </div>
+            );
+        }
+        return null;
+    };
+
+    const CustomCursor = ({ points }) => {
+        return <Rectangle fill='#000000' opacity={0.2} x={points[0].x} width={500} height={300} />;
+    };
         return (
-            <ResponsiveContainer width="100%" height="100%">
+            <ResponsiveContainer width="115%" height="100%" >
                 <LineChart
                     data={sessions}
                     margin={{
-                        top: 30,
-                        right: 0,
-                        left: 0,
-                        bottom: 5,
+                        top: 20,
+                        right: 5,
+                        left: 5,
+                        bottom: 15,
                     }}
                 >
-                    <XAxis dataKey="day" tickLine={false} axisLine={false} tick={{fill: 'rgba(255, 255, 255, 50%)',stroke: 'rgba(255, 255, 255, 50%)', strokeWidth: 1}} />
-                    <YAxis  hide={true}/>
-                    <Tooltip />
-                    <Line type="monotone" dot={false} dataKey="value" stroke="white" strokeWidth={2} activeDot={activeDotStyle} />
+                    <defs>
+                        <linearGradient id='sessionLine' x1='0%' y1='0' x2='100%' y2='0'>
+                            <stop offset='0%' stopColor='rgba(255, 255, 255, 0.3)'/>
+                            <stop offset='20%' stopColor='rgba(255, 255, 255, 0.4)'/>
+                            <stop offset='40%' stopColor='rgba(255, 255, 255, 0.5)'/>
+                            <stop offset='60%' stopColor='rgba(255, 255, 255, 0.6)'/>
+                            <stop offset='100%' stopColor='rgba(255, 255, 255, 1)'/>
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="day" tickLine={false} axisLine={false} tick={{
+                        fill: 'rgba(255, 255, 255, 50%)',
+                        stroke: 'rgba(255, 255, 255, 50%)',
+                        strokeWidth: 1
+                    }}/>
+                    <YAxis hide={true}/>
+                    <Tooltip content={<CustomTooltip />} cursor={<CustomCursor />}/>
+                    <Line type="monotone" dot={false} dataKey="value" stroke='url(#sessionLine)'
+                          strokeWidth={2}
+                          activeDot={activeDotStyle}/>
                 </LineChart>
             </ResponsiveContainer>
         );
